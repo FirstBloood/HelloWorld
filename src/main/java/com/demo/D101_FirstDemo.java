b3e6c6b3e3f68096240b17ebd88a6ff66c83a080
Bruce <CheFB_2014@163.com>
2017-06-29 21:46:44 +0800

    test in home

--- src/main/java/com/demo/D101_FirstDemo.java
+++ src/main/java/com/demo/D101_FirstDemo.java
@@ -4,6 +4,7 @@ public class D101_FirstDemo {
 	public static void main(String[] args) {
 		System.out.println("firstB");
 		System.out.println("blood");
+		System.out.println("commit in hone");
 	}
 
 }
